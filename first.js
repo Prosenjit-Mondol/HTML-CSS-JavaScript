ba6ff5f96c6f649f2da0546b0dc821349f88bf6c
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-11-22 16:53:31 +0600

    11/22/24

--- first.js
+++ first.js
@@ -28,3 +28,19 @@ let str=["Prosenjit","Pritha","Aishariya","Tofha"];
 str.forEach((val,idx) =>{
     console.log(idx,val);
 });
+
+console.dir(document.body);
+
+let btn1=document.querySelector("#btn1");
+
+btn1.onclick = () => {
+    console.log("btn1 was clicked");
+    let a=25;
+    a++;
+    console.log(a)
+}
+
+let div=document.querySelector("div");
+div.onmouseover=()=>{
+    console.log("YOU ArE inside box")
+}
