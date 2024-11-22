ce2dc23cb86fa1d37e7c3da7a9565f7923310eff
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-11-22 11:09:11 +0600

    11/22/24

--- first.js
+++ first.js
@@ -1,1 +1,15 @@
 console.log("Welcome PROSENJIT MONDOL");
+const age=23;
+x=null;
+y=undefined;
+console.log(age,x,y);
+
+let name="Pritha";
+console.log(name)
+
+let marks=[85,95,89,93,92];
+let sum=0;
+for (let index = 0; index < marks.length; index++) {
+    sum+= marks[index];
+}
+console.log("The total sum of the marks is ",sum);
